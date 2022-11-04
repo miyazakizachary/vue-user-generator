9d699565c731ce54a008f39810c8388637d3322b
mac <chefarchitekt@live.com>
2022-11-04 16:28:06 +0800

    finish project

---
+++ app.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    },
+    /*
+    methods: {
+        getUser() {
+            this.firstName = 'Elizabeth',
+            this.lastName = 'Sawyer',
+            this.email = 'sm@gmail.com',
+            this.gender = 'female',
+            this. picture= 'https://randomuser.me/api/portraits/women/10.jpg'
+        }
+    }
+    */
+   methods: {
+    async getUser() {
+        const response = await fetch('https://randomuser.me/api')
+        const { results } = await response.json()
+        console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this. picture = results[0].picture.large
+    }
+   }
+})
+
+app.mount('#app')
